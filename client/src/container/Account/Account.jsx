e1fbe20ec89aa6d5ab667e481a98d480e6d4b549
Yurii <yur.dub7@gmail.com>
2021-01-11 17:11:40 +0200

    add pretier and eslint
    add libs to configure
    change js => jsx

---
+++ client/src/container/Account/Account.jsx
@@ -0,0 +1,123 @@
+import React, { useState, useEffect } from "react";
+import AccountInfo from "../../components/AccountInfo/AccountInfo";
+import AccountOrders from "../../components/AccountOrders/AccountOrders";
+import AccountSettings from "../../components/AccountSettings/AccountSettings";
+import AccountSwitchBox from "../../components/AccountSwitchBox/AccountSwitchBox";
+import TitlePager from "../../components/TitlePager/TitlePager";
+import "./Account.css";
+import { useSelector, useDispatch } from "react-redux";
+import API from "../../API/API";
+import { addUserSuccess } from "../../store/actions/actionsUser";
+import { useHistory } from "react-router-dom";
+
+const Account = () => {
+    const dispatch = useDispatch();
+    const { email, name, surname, gender, age, phone, id, emailVerify, auth } = useSelector((state) => state.user.user);
+
+    const history = useHistory();
+    const [data, setData] = useState(false);
+    useEffect(() => {
+        if (!auth) {
+            history.push("/");
+        } else {
+            API.get("/user/getCountSellProducts/" + email).then((res) => {
+                setData(res.data.data);
+            });
+        }
+    }, [auth]);
+
+    const [showInfo, setShowInfo] = useState(true);
+    const [showSettings, setShowSetings] = useState(false);
+    const [showOrder, setShowOrder] = useState(false);
+
+    const showInfoBox = () => {
+        setShowInfo(true);
+        setShowOrder(false);
+        setShowSetings(false);
+    };
+    const showSettingsBox = () => {
+        setShowInfo(false);
+        setShowOrder(false);
+        setShowSetings(true);
+    };
+    const showOrderBox = () => {
+        setShowInfo(false);
+        setShowOrder(true);
+        setShowSetings(false);
+    };
+
+    const [newName, setNewName] = useState(name);
+    const [newSurname, setNewSurname] = useState(surname);
+    const [newGender, setNewGender] = useState(gender);
+    const [newAge, setNewAge] = useState(age);
+    const [newPhone, setNewPhone] = useState(phone);
+
+    const saveChange = () => {
+        const user = {
+            name: newName || name,
+            surname: newSurname || surname,
+            gender: newGender || gender,
+            age: newAge || age,
+            phone: newPhone || phone,
+            id: id,
+        };
+
+        API.put("/user/changeInfo", user).then((res) => {
+            dispatch(addUserSuccess(res.data.data));
+        });
+    };
+
+    const [sendEmail, setSetEmail] = useState(false);
+
+    const goVerify = () => {
+        setSetEmail(true);
+        API.post("/user/emailVerify", { email, id });
+    };
+
+    return (
+        <div className="account-wrapper">
+            <TitlePager title="Account" />
+            <div className="account__container account">
+                <AccountSwitchBox
+                    showInfo={showInfo}
+                    showSettings={showSettings}
+                    showOrder={showOrder}
+                    showInfoBox={showInfoBox}
+                    showSettingsBox={showSettingsBox}
+                    showOrderBox={showOrderBox}
+                />
+
+                {showInfo ? (
+                    <AccountInfo email={email} name={name} surname={surname} gender={gender} age={age} phone={phone} />
+                ) : (
+                    <></>
+                )}
+                {showSettings ? (
+                    <AccountSettings
+                        email={email}
+                        emailVerify={emailVerify}
+                        sendEmail={sendEmail}
+                        newPhone={newPhone}
+                        newName={newName}
+                        newSurname={newSurname}
+                        newGender={newGender}
+                        newAge={newAge}
+                        setNewName={(e) => setNewName(e.target.value)}
+                        setNewSurname={(e) => setNewSurname(e.target.value)}
+                        setNewGender={(e) => setNewGender(e.target.value)}
+                        setNewAge={(e) => setNewAge(Number(e.target.value))}
+                        setNewPhone={(e) => setNewPhone(e.target.value)}
+                        saveChange={saveChange}
+                        goVerify={goVerify}
+                    />
+                ) : (
+                    <></>
+                )}
+                {showOrder ? <AccountOrders data={data} /> : <></>}
+            </div>
+        </div>
+    );
+};
+
+Account.whyDidYouRender = true;
+export default React.memo(Account);
--- client/src/store/actions/actionsUser.js
+++ client/src/store/actions/actionsUser.js
@@ -1,133 +1,125 @@
-import * as actionTypes from './actionTypes';
-import API from './../../API/API'
+import * as actionTypes from "./actionTypes";
+import API from "./../../API/API";
 
 export const logout = () => {
-  
-  return function (dispatch) {
-    dispatch(removeUser())
-    return API.get('/auth/signout')
-  }
-}
-
-
-export const fetchUserSign = (email,password,name) => {
     return function (dispatch) {
-      dispatch(addUser())
-      return API.post('/auth/sign',{email,password,name,username:'q'})
-        .then( (user) => {
-            if(user.data.err){
-              dispatch(addUserFail(user.data.errMess))
-            }else{
-              dispatch(addUserSuccess({
-                email:user.data.data.email,
-                likeProducts:user.data.data.likeProducts,
-                id:user.data.data._id,
-                gender:user.data.data.gender,
-                name:user.data.data.name,
-                surname:user.data.data.surname,
-                phone:user.data.data.phone,
-                age:user.data.data.age,
-                emailVerify:user.data.data.emailVerify
-              }))
+        dispatch(removeUser());
+        return API.get("/auth/signout");
+    };
+};
+
+export const fetchUserSign = (email, password, name) => {
+    return function (dispatch) {
+        dispatch(addUser());
+        return API.post("/auth/sign", { email, password, name, username: "q" }).then((user) => {
+            if (user.data.err) {
+                dispatch(addUserFail(user.data.errMess));
+            } else {
+                dispatch(
+                    addUserSuccess({
+                        email: user.data.email,
+                        likeProducts: user.data.likeProducts,
+                        id: user.data._id,
+                        gender: user.data.gender,
+                        name: user.data.name,
+                        surname: user.data.surname,
+                        phone: user.data.phone,
+                        age: user.data.age,
+                        emailVerify: user.data.emailVerify,
+                    })
+                );
             }
-        })
-
-    }
-  }
-export const fetchUserLogin = (email,password) => {
-
+        });
+    };
+};
+export const fetchUserLogin = (email, password) => {
     return function (dispatch) {
-      dispatch(addUser())
-      return API.post('/auth/login',{email,password,username:'q'})
-        .then( (user) => {
-          if(user.data.err){
-            dispatch(addUserFail(user.data.errMess))
-          }else{
-            dispatch(addUserSuccess({
-                email:user.data.data.email,
-                likeProducts:user.data.data.likeProducts,
-                id:user.data.data._id,
-                gender:user.data.data.gender,
-                name:user.data.data.name,
-                surname:user.data.data.surname,
-                phone:user.data.data.phone,
-                age:user.data.data.age,
-                emailVerify:user.data.data.emailVerify
-			      }))
-          }
-        })
-      
-    }
-}
+        dispatch(addUser());
+        return API.post("/auth/login", { email, password, username: "q" }).then((user) => {
+            if (user.data.err) {
+                dispatch(addUserFail(user.data.errMess));
+            } else {
+                dispatch(
+                    addUserSuccess({
+                        email: user.data.email,
+                        likeProducts: user.data.likeProducts,
+                        id: user.data._id,
+                        gender: user.data.gender,
+                        name: user.data.name,
+                        surname: user.data.surname,
+                        phone: user.data.phone,
+                        age: user.data.age,
+                        emailVerify: user.data.emailVerify,
+                    })
+                );
+            }
+        });
+    };
+};
 
 export const addUser = () => {
     return {
-      type: actionTypes.ADD_USER,
-    }
-  }
+        type: actionTypes.ADD_USER,
+    };
+};
 export const addUserFail = (mess) => {
     return {
-      type: actionTypes.ADD_USER_FAIL,
-      mess
-    }
-  }
-  export const addUserSuccess = (user) => {
+        type: actionTypes.ADD_USER_FAIL,
+        mess,
+    };
+};
+export const addUserSuccess = (user) => {
     return {
-      type: actionTypes.ADD_USER_SUCCESS,
-      user
-    }
-  }
+        type: actionTypes.ADD_USER_SUCCESS,
+        user,
+    };
+};
 export const removeUser = () => {
     return {
-      type: actionTypes.CLEAR_USER,
-  
-  }
-}
+        type: actionTypes.CLEAR_USER,
+    };
+};
 
-export const fetchAddLikeProduct = (id,newLikeProducts) => {
-    
+export const fetchAddLikeProduct = (id, newLikeProducts) => {
     return function (dispatch) {
-      dispatch(addLikeProduct())
-      return API.put('/user/addProduct',{id,newLikeProducts})
-        .then( (user) => {
-            if(user.data.err){
-              dispatch(addLikeProductFail(user.data.errMess))
-            }else{
-              dispatch(addLikeProductrSuccess(user.data.data.likeProducts))
+        dispatch(addLikeProduct());
+        return API.put("/user/addProduct", { id, newLikeProducts }).then((user) => {
+            if (user.data.err) {
+                dispatch(addLikeProductFail(user.data.errMess));
+            } else {
+                dispatch(addLikeProductrSuccess(user.data.data.likeProducts));
             }
-        })
-
-    }
-  }
-
+        });
+    };
+};
 
 export const addLikeProduct = () => {
-  return {
-    type: actionTypes.ADD_LIKE_PRODUCT,
-  }
-}
+    return {
+        type: actionTypes.ADD_LIKE_PRODUCT,
+    };
+};
 export const addLikeProductFail = (mess) => {
     return {
-      type: actionTypes.ADD_LIKE_PRODUCT_FAIL,
-      mess
-    }
-  }
+        type: actionTypes.ADD_LIKE_PRODUCT_FAIL,
+        mess,
+    };
+};
 export const addLikeProductrSuccess = (data) => {
     return {
-      type: actionTypes.ADD_LIKE_PRODUCT_SUCCESS,
-      data
-    }
-}
+        type: actionTypes.ADD_LIKE_PRODUCT_SUCCESS,
+        data,
+    };
+};
 
 export const addBuyProduct = (data) => {
-  return {
-    type: actionTypes.ADD_BUY_PRODUCT,
-    data
-  }
-}
+    return {
+        type: actionTypes.ADD_BUY_PRODUCT,
+        data,
+    };
+};
 
 export const clearBucketUser = () => {
-  return {
-    type: actionTypes.CLEAR_BUCKET_USER,
-  }
-}
+    return {
+        type: actionTypes.CLEAR_BUCKET_USER,
+    };
+};
