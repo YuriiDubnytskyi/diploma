072e7a28e4674a0a4d4a68bf926ca64995aa4b96
Yurii <yur.dub7@gmail.com>
2021-02-22 08:43:57 +0200

    add some upgrate to speed test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
     return (
         <BrowserRouter>
             <Header />
-            <Suspense>
+            <Suspense fallback={<div>Loading ...</div>}>
                 <Main />
             </Suspense>
             <Footer />
