109bc0548e6c8b7746a265a2d8318990f3cbefc0
Yurii <yur.dub7@gmail.com>
2021-04-02 18:12:00 +0300

    add sitemap genertor

---
+++ client/src/routes.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+const Routes = () => {
+    return (
+        <Switch>
+            <Route path="/" />
+            <Route path="/categorie" />
+            <Route path="/productList/:id/:name" />
+            <Route path="/product/:id/:name/:productName" />
+            <Route path="/contact" />
+            <Route path="/login" />
+            <Route path="/blog" />
+            <Route path="/account" />
+            <Route path="/like" />
+            <Route path="/bucket" />
+            <Route path="/newsID/:id" />
+            <Route path="/search/:text" />
+            <Route path="/buyProduct" />
+            <Route path="/callback/:id" />
+        </Switch>
+    );
+};
+export default Routes;
